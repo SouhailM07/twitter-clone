28a2627559653afeb23c84d5319baef0e3ca1b36
souhail <lightshadow416@gmail.com>
2024-02-14 18:57:40 +0100

    [FollowPanel] component checkpoint {1}

--- components/FollowPanel/FollowPanel.tsx
+++ components/FollowPanel/FollowPanel.tsx
@@ -2,7 +2,18 @@ import "./followpanel.css";
 export default function FollowPanel() {
   return (
     <>
-      <article>FollowPanel</article>
+      <aside className="min-w-[21rem] border-2 p-[1.5rem]">
+        <h1 className="text-[2rem] ">Who to follow</h1>
+        <ul></ul>
+      </aside>
     </>
   );
 }
+
+let Follow = () => {
+  return (
+    <>
+      <li></li>
+    </>
+  );
+};
