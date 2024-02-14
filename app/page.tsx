8984a5dc13f0a261e570a3c1689b7ccb9939abf4
souhail <lightshadow416@gmail.com>
2024-02-14 19:57:30 +0100

    [WelcomePanel] component was created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 // components
 import TweetPanel from "@/components/TweetPanel/TweetPanel";
+import WelcomePanel from "@/components/WelcomePanel/WelcomePanel";
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen border border-gray-600">
       <h1 className="route__title ">Home</h1>
-      <TweetPanel />
+      {/* <TweetPanel /> */}
+      <WelcomePanel />
     </main>
   );
 }
--- components/WelcomePanel/WelcomePanel.tsx
+++ components/WelcomePanel/WelcomePanel.tsx
@@ -1,4 +1,17 @@
 import "./welcomepanel.css";
+
 export default function WelcomePanel() {
-  return <>WelcomePanel</>;
+  return (
+    <div className="text-center ">
+      <h1 className="text-[2rem] my-[1rem]">Welcome to Twitter</h1>
+      <div className="font-medium space-x-[1rem]">
+        <button className="bg-btnColor__disable px-[1.5rem] py-[0.8rem] rounded-full hover:bg-btnColor__active">
+          Login
+        </button>
+        <button className="text-black bg-gray-200 rounded-full px-[1.5rem] py-[0.8rem] hover:bg-white">
+          SignIn
+        </button>
+      </div>
+    </div>
+  );
 }
